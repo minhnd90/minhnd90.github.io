86774b940a7746b76662411b8d2d1a7da5e194c8
minhnd90 <Mr.Lonely905B@gmail.com>
2026-01-12 16:03:01 +0700

    feat: Refactor layout, footer, navbar, and theme switcher components; update styles and dependencies

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,16 +4,22 @@ const YEAR = new Date().getFullYear()
 export default function Footer() {
   return (
     <>
-      <small className='footer'>
-        <time>{YEAR}</time> © CÔNG TY TNHH DỊCH VỤ VIỆC LÀM BÌNH MINH GROUP.
-        <Link href="tel:+84988108250">
-          0988.108.250
-        </Link>
-        <Link href="tel:+84385616604">
-          0385.616.604
-        </Link>
-        <a href="/feed.xml">RSS</a>
-      </small>
+      <div className='footer'>
+        <div className="footer-container">
+          <div className="col-left">
+            <time>{YEAR}</time> © {process.env.COMPANY_NAME}
+          </div>
+          <div className="col-right">
+            <Link href="tel:+84988108250">
+              0988.108.250
+            </Link>
+            <Link href="tel:+84385616604">
+              0385.616.604
+            </Link>
+            <a href="/feed.xml">RSS</a>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
